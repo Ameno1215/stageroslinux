f0a7db3062f13cc6ab09c5dd77484bfa6b859d36
Antonin Chauvet <antochauvet@gmail.com>
2026-04-13 14:22:12 +0200

    joint monitor

---
+++ denso_ros2_ws/src/joint_monitor/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'joint_monitor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='antonin',
+    maintainer_email='antochauvet@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'joint_monitor_node = joint_monitor.joint_monitor_node:main',
+        ],
+    },
+)
