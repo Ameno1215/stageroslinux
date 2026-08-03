e231d63a095e4ab159aff7636212b2b5f0b22d10
Antonin CHAUVET <antonin.chauvet@ingroupe.com>
2026-08-03 09:54:30 +0200

    refactor denso_ros2_ws to ros2_ws

--- denso_ros2_ws/src/denso_robot_drivers_ros2/build/denso_robot_descriptions/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/antonin/workspace/denso_ros2_ws/src/denso_robot_drivers_ros2/denso_robot_descriptions
-# Build directory: /home/antonin/workspace/denso_ros2_ws/src/denso_robot_drivers_ros2/build/denso_robot_descriptions
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
